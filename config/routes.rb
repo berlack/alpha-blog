009569aac768a1490ac4c0dadc346300669ac4ee
Berke Ucal <berkeucal@gmail.com>
2018-02-16 21:01:32 +0300

    signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "pages#homepage"
-  get "about", to:"pages#about"
-  get "sevr", to:"pages#sevr"
+  get "about", to: "pages#about"
+  get "sevr", to: "pages#sevr"
 
   resources :articles
+
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
+  # post "users", to: "users#create"
 
 end
